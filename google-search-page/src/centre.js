ad220f10cbcee1041e6e3fab0a0af02549b95085
Milad-Rufai <temilader19@gmail.com>
2023-04-10 12:25:53 +0100

    Updates

--- google-search-page/src/centre.js
+++ google-search-page/src/centre.js
@@ -3,22 +3,36 @@ import './Centre.css'
 import logo from './images/logo.png'
 import mic from './images/mic.png'
 import imagesearch from './images/imagesearch.png'
+import { useState } from 'react';
+
+
 
 function Centre() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [listItems, setListItems] = useState([]);
   return (
     <div className='Centre'>
-    <img className='Centre-logo' src={logo} alt='logo'></img>
-    <br></br>
-    <div className='search-area'>
-    <input className='search' type='search bar'></input>
-    <img className='icon search-icon icn1' src={mic} alt='logo'></img>
-    <img className='icon search-icon icn2' src={imagesearch} alt='logo'></img>
-    </div>
-    <div>
+      <img className='Centre-logo' src={logo} alt='logo'></img>
+      <br></br>
+        <form onSubmit={(event) => {
+          event.preventDefault();
+          setListItems([...listItems, searchTerm]);
+          setSearchTerm('');
+        }}>
+        <input className='search' type='search-bar'></input>
+        <img className='search-icon icn1' src={mic} alt='logo'></img>
+        <img className='search-icon icn2' src={imagesearch} alt='logo'></img>
+        </form>
+      <ul className='search-list'>
+        {listItems.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <div>
         <button className='btn' href='#'>Google Search</button>
         <button className='btn' href='#'>I'm Feeling Lucky</button>
-    </div>
-    <div className='languages' >Google offered in: <a href='#'>Hausa</a> <a href='#'>Igbo</a> <a href='#'>Èdè Yorùbá</a> <a href='#'>Nigerian Pidgin</a></div>
+      </div>
+      <div className='languages' >Google offered in: <a href='#'>Hausa</a> <a href='#'>Igbo</a> <a href='#'>Èdè Yorùbá</a> <a href='#'>Nigerian Pidgin</a></div>
     </div>
   )
 }
